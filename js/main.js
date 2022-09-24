6cdc6f8a710cb3cc2b2a09d31e38b06413d6c1f1
Maksym Solomko <max.exc7@gmail.com>
2022-09-24 07:42:24 +0300

    Cambodia)

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function parallax(event) {
+  //   console.log(event);
+  this.querySelectorAll(".layer").forEach((layer) => {
+    let speed = layer.getAttribute("data-speed");
+    console.log(layer.getAttribute("data-speed"));
+    layer.style.transform = `translateX(${(event.clientX * speed) / 1200}px)`;
+  });
+}
+document.addEventListener("mousemove", parallax);
+// document.querySelectorAll(".layer");
+transform.translate;
