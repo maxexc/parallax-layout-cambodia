919d82472f2ef2d1190aaa1b42609389a67863fc
Maksym Solomko <max.exc7@gmail.com>
2022-10-02 23:11:50 +0300

    touchmove yoo-hoo!))

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 function parallax(event) {
-  //   console.log(event);
+  // console.log(event);
   this.querySelectorAll(".layer").forEach((layer) => {
     let speed = layer.getAttribute("data-speed");
     // console.log(layer.getAttribute("data-speed"));
     layer.style.transform = `translateX(${(event.clientX * speed) / 2000}px)`;
+    layer.style.transform = `translateX(${
+      (event.changedTouches[0].pageX * speed) / 2000
+    }px)`;
   });
 }
 document.addEventListener("mousemove", parallax);
+document.addEventListener("touchmove", parallax); // yoo-hoo!!))) It`s works!
