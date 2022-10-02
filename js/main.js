3f5898eea20feee1392cfe1f6910796466f8d508
Maksym Solomko <max.exc7@gmail.com>
2022-10-02 23:22:27 +0300

    up

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function parallax(event) {
     // console.log(layer.getAttribute("data-speed"));
     layer.style.transform = `translateX(${(event.clientX * speed) / 2000}px)`;
     layer.style.transform = `translateX(${
-      (event.changedTouches[0].pageX * speed) / 2000
+      (event.changedTouches[0].pageX * speed) / 1000
     }px)`;
   });
 }
