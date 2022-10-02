8223527da03429690f1e1a5810258a9062c2d032
Maksym Solomko <max.exc7@gmail.com>
2022-10-02 23:26:34 +0300

    up

--- js/main.js
+++ js/main.js
@@ -4,10 +4,23 @@ function parallax(event) {
     let speed = layer.getAttribute("data-speed");
     // console.log(layer.getAttribute("data-speed"));
     layer.style.transform = `translateX(${(event.clientX * speed) / 2000}px)`;
+    // layer.style.transform = `translateX(${
+    //   (event.changedTouches[0].pageX * speed) / 1000
+    // }px)`;
+  });
+}
+
+document.addEventListener("mousemove", parallax);
+document.addEventListener("touchmove", parallaxTouch); // yoo-hoo!!))) It`s works!
+
+function parallaxTouch(event) {
+  // console.log(event);
+  this.querySelectorAll(".layer").forEach((layer) => {
+    let speed = layer.getAttribute("data-speed");
+    // console.log(layer.getAttribute("data-speed"));
+    // layer.style.transform = `translateX(${(event.clientX * speed) / 2000}px)`;
     layer.style.transform = `translateX(${
       (event.changedTouches[0].pageX * speed) / 1000
     }px)`;
   });
 }
-document.addEventListener("mousemove", parallax);
-document.addEventListener("touchmove", parallax); // yoo-hoo!!))) It`s works!
