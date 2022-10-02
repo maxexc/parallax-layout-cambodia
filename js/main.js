1ddb2a3125c633b22068447064d8c06384842888
Maksym Solomko <max.exc7@gmail.com>
2022-10-03 00:31:26 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,9 +4,6 @@ function parallax(event) {
     let speed = layer.getAttribute("data-speed");
     // console.log(layer.getAttribute("data-speed"));
     layer.style.transform = `translateX(${(event.clientX * speed) / 2000}px)`;
-    // layer.style.transform = `translateX(${
-    //   (event.changedTouches[0].pageX * speed) / 1000
-    // }px)`;
   });
 }
 
@@ -14,11 +11,8 @@ document.addEventListener("mousemove", parallax);
 document.addEventListener("touchmove", parallaxTouch); // yoo-hoo!!))) It`s works!
 
 function parallaxTouch(event) {
-  // console.log(event);
   this.querySelectorAll(".layer").forEach((layer) => {
     let speed = layer.getAttribute("data-speed");
-    // console.log(layer.getAttribute("data-speed"));
-    // layer.style.transform = `translateX(${(event.clientX * speed) / 2000}px)`;
     layer.style.transform = `translateX(${
       (event.changedTouches[0].pageX * speed) / 1000
     }px)`;
